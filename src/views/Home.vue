66baef3a3c2560da7a2d77ca65dd15977e493aa5
CrisEspinoza <cristian.espinoza.s@usach.cl>
2020-03-30 22:03:03 -0300

    ADD: Adding the principal components

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/global/Header";
+import Body from "@/components/global/Body";
+import Footer from "@/components/global/Footer";
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Body,
+    Footer,
+  },
+};
+</script>
